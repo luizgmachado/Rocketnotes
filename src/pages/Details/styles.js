b7def9f79d5998421e7d8d9ba8c25928ae439928
luizgmachado <luiz.gustavo.machado@hotmail.com>
2023-04-25 16:24:18 -0300

    install style componets

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,3 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  font-size: 1.6rem;
+  font-family: "Roboto", sans-serif;
+  background: #0e0e0e;
+`
+
+
+
+
+
+/*
 * {
   margin: 0;
   padding: 0;
@@ -19,4 +32,4 @@ body {
 h1 {
   font-size: 3.2rem;
   color: #fff;
-}
+}*/
