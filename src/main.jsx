4818b64028f37d8f9aabdf1dc54b632f4f8599b3
luizgmachado <luiz.gustavo.machado@hotmail.com>
2023-04-25 21:03:46 -0300

    app thme global

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { Details } from "./pages/Details"
 import { ThemeProvider } from "styled-components"
+import GlobalStyle from "./styles/global"
 import theme from "./styles/theme"
+
+import { Details } from "./pages/Details"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode >
 		<ThemeProvider theme={theme}>
+      <GlobalStyle/>
 			<Details />
 		</ThemeProvider>
 	</React.StrictMode>
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -3,33 +3,4 @@ import styled from 'styled-components'
 export const Container = styled.div`
   font-size: 1.6rem;
   font-family: "Roboto", sans-serif;
-  background: #0a1b;
 `
-
-
-
-
-
-/*
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-:root {
-  font-size: 62.5%;
-}
-
-body {
-  font-size: 1.6rem;
-  font-family: "Roboto", sans-serif;
-  background: #0e0e0e;
-}
-
-h1 {
-  font-size: 3.2rem;
-  color: #fff;
-}*/
