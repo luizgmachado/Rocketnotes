4765a7b0ba05595b85dca4c0b148ebeb77e4fe44
luizgmachado <luiz.gustavo.machado@hotmail.com>
2023-04-27 18:01:55 -0300

    new components Children

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,21 @@
-import { Container, Profile } from "./styles";
+import { RiShutDownLine } from "react-icons/ri"
+import { Container, Profile, Logout } from "./styles"
 
 export function Header() {
-  return (
-    <Container>
-      <Profile>
-        <img src="https://github.com/luizgmachado.png" alt="Foto do Usuário" />
+	return (
+		<Container>
+			<Profile>
+				<img src="https://github.com/luizgmachado.png" alt="Foto do Usuário" />
 
-        <div>
-          <span>Bem vindo,</span>
-          <strong>Luiz Machado</strong>
-        </div>
-      </Profile>
-    </Container>
-  );
+				<div>
+					<span>Bem vindo,</span>
+					<strong>Luiz Machado</strong>
+				</div>
+			</Profile>
+
+			<Logout>
+				<RiShutDownLine />
+			</Logout>
+		</Container>
+	)
 }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,25 @@
-import { Container } from './styles'
+import { Container, Links } from './styles'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 
 export function Details() {
   return (
 		<Container>
 			<Header />
 
-			<Button title="Voltar" loading={false}/>
+			<Section title="Links úteis">
+				<Links>
+					<li>
+						<a href="#">https://www.rocketseat.com.br/</a>
+					</li>
+					<li>
+						<a href="#">https://www.rocketseat.com.br/</a>
+					</li>
+				</Links>
+			</Section>
+
+			<Button title="Voltar" loading={false} />
 		</Container>
 	)
 }
