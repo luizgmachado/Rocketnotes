9221d4cd4764bbf3cd323962fe4757e81b2ef5b7
luizgmachado <luiz.gustavo.machado@hotmail.com>
2023-05-11 16:42:29 -0300

    upload form to pages profile

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import { ThemeProvider } from "styled-components"
 import GlobalStyle from "./styles/global"
 import theme from "./styles/theme"
 
-// import { Details } from "./pages/Details"
-import { SignUp } from "./pages/SignUp"
+
+import { Profile } from "./pages/Profile"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
-			<SignUp/>
+			<Profile/>
 		</ThemeProvider>
 	</React.StrictMode>
 )
