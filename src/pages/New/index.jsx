957a36a51cb5f14d6c9dafb4eb72cb38fc268bb0
luizgmachado <luiz.gustavo.machado@hotmail.com>
2023-05-12 15:55:18 -0300

    interface New

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+
+import { Header } from "../../components/Header";
+
+export function New() {
+  return(
+    <Container>
+      <Header />
+
+      
+    </Container>
+  )
+}
