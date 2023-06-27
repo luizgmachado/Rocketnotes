14bbe811116a97edaccff3f21f59fc94ca79aee3
luizgmachado <luiz.gustavo.machado@hotmail.com>
2023-06-27 17:55:10 -0300

    signIn e singOut

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,18 +1,21 @@
-import { createContext, useContext, useState } from "react";
-import { api } from '../services/api'
+/** @format */
 
+import { createContext, useContext, useState, useEffect } from "react"
+import { api } from "../services/api"
 
 export const AuthContext = createContext({})
 function AuthProvider({ children }) {
 	const [data, setData] = useState({})
- async function signIn({ email, password }) {
+	async function signIn({ email, password }) {
 		try {
 			const response = await api.post("/sessions", { email, password })
 			const { token, user } = response.data
 
-			api.defaults.headers.authorization = `Bearer ${token}`	
+			localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+			localStorage.setItem("@rocketnotes:token", token)
+
+			api.defaults.headers.authorization = `Bearer ${token}`
 			setData({ token, user })
-			
 		} catch (error) {
 			if (error.response) {
 				alert(error.response.message)
@@ -20,21 +23,43 @@ function AuthProvider({ children }) {
 				alert("Não foi possível entrar.")
 			}
 		}
- }
+	}
 
-  return (
-    <AuthContext.Provider value={{ signIn, user: data.user }}>
-      {children}
-    </AuthContext.Provider>
-  )
+	function signOut() {
+		localStorage.removeItem("@rocketnotes:user")
+		localStorage.removeItem("@rocketnotes:token")
+
+		setData({})
+	}
+
+	useEffect(() => {
+		const user = localStorage.getItem("@rocketnotes:user")
+		const token = localStorage.getItem("@rocketnotes:token")
+
+		if (token && user) {
+			api.defaults.headers.authorization = `Bearer ${token}`
+			setData({
+				token,
+				user: JSON.parse(user),
+			})
+		}
+	}, [])
+
+	return (
+		<AuthContext.Provider
+			value={{
+				signIn,
+				signOut,
+				user: data.user,
+			}}>
+			{children}
+		</AuthContext.Provider>
+	)
 }
 
 function useAuth() {
-  const context = useContext(AuthContext)
-  return context
+	const context = useContext(AuthContext)
+	return context
 }
 
-export { 
-  AuthProvider,
-  useAuth
-}
+export { AuthProvider, useAuth }
