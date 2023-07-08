02abf3cacd72f55703f69795e62cd8e9e93846b7
luizgmachado <luiz.gustavo.machado@hotmail.com>
2023-07-07 21:19:17 -0300

    Carregando detalhes da nota

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,53 +1,77 @@
-import { Container, Links, Content } from './styles'
-import { Button } from '../../components/Button'
-import { ButtonText } from '../../components/ButtonText'
-import { Header } from '../../components/Header'
-import { Section } from '../../components/Section'
-import { Tag } from '../../components/Tag'
+import { useState, useEffect } from "react"
+import { Container, Links, Content } from "./styles"
+import { useParams } from "react-router-dom"
 
+import { api } from "../../services/api"
+
+import { Button } from "../../components/Button"
+import { ButtonText } from "../../components/ButtonText"
+import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
 
 export function Details() {
-  return (
+	const [data, setData] = useState(null)
+
+	const params = useParams()
+
+	useEffect(() => {
+		async function fetchData() {
+			const response = await api.get(`/notes/${params.id}`)
+			setData(response.data)
+		}
+		fetchData()
+	}, [])
+
+	return (
 		<Container>
 			<Header />
 
-			<main>
-				<Content>
-					<ButtonText title="Excluir Nota" />
+			{data && (
+				<main>
+					<Content>
+						<ButtonText title="Excluir Nota" />
 
-					<h1>Introdução ao React</h1>
+						<h1 >{data.title}</h1>
 
-					<p>
-						React é uma biblioteca JavaScript de código aberto usada para
-						construir interfaces de usuário. Ela foi criada pelo Facebook e
-						lançada em 2013. Desde então, tornou-se uma das bibliotecas mais
-						populares para o desenvolvimento de aplicativos web e móveis. O
-						React é baseado em componentes, o que significa que as interfaces
-						são divididas em partes menores e independentes chamadas de
-						componentes. Cada componente pode ter seu próprio estado e
-						comportamento, permitindo que os desenvolvedores construam
-						interfaces complexas e interativas de forma modular e escalável.
-					</p>
+						<p>{data.description}</p>
 
-					<Section title="Links úteis">
-						<Links>
-							<li>
-								<a href="#">https://www.rocketseat.com.br/</a>
-							</li>
-							<li>
-								<a href="#">https://www.rocketseat.com.br/</a>
-							</li>
-						</Links>
-					</Section>
-
-					<Section title="Marcadores">
-						<Tag title="Express" />
-						<Tag title="NodeJS" />
-					</Section>
-
-					<Button title="Voltar" loading={false} to="/" />
-				</Content>
-			</main>
+						{
+						
+						data.links && 
+							<Section title="Links úteis">
+								<Links>
+									{
+									data.links.map((link) => (
+										<li 
+											key={String(link.id)}>
+											<a 
+											target="_blank"
+											href={link.url}>{link.url}
+											
+											</a>
+										</li>
+									))
+									}
+								</Links>
+							</Section>
+						}
+						{
+						data.tags && 
+							<Section title="Marcadores">
+								{
+								data.tags.map((tag) => (
+									<Tag 
+										key={String(tag.id)}
+										title={tag.name} />
+								))
+								}
+							</Section>
+						}
+						<Button title="Voltar" />
+					</Content>
+				</main>
+			)}
 		</Container>
 	)
 }
